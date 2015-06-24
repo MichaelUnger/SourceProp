151799eecadee5f073c19e3de9db8f331d124435
Michael Unger <11771724+MichaelUnger@users.noreply.github.com>
2015-06-24 04:01:15 +0000

    sh-ify
    git-svn-id: https://svn.iap.kit.edu/svn/users/munger/GMF/Prop@3548 b4666b34-504e-453f-89ac-0a9f10f0e403

--- bin/subPropmatrix.sh
+++ bin/subPropmatrix.sh
@@ -1,23 +1,22 @@
-#!/bin/tcsh 
+#!/bin/tcsh
 
-set options = "SFR2 uniform uniformCutAt3 AGN AAGHRW05 M10 M15 M20 M25 M30 M35 M40 M45 M50"
+#options = "SFR2 uniform uniformCutAt3 AGN AAGHRW05 M10 M15 M20 M25 M30 M35 M40 M45 M50"
 
-set options = "uniform"
-
-setenv EXEDIR /home/mu495/Software/Prop
-setenv PIONFILE $EXEDIR/bin/pionDecay.root
-setenv PHOTONFIELD Test #CRPropaG12
-setenv OUTDIR /scratch/mu495/Matrices
-setenv INDIR /scratch/mu495/
+option="uniform"
+export EXEDIR=/home/mu495/Software/Prop
+export PIONFILE=$EXEDIR/bin/pionDecay.root
+export PHOTONFIELD=Test #CRPropaG12
+export OUTDIR=/scratch/mu495/Matrices
+export INDIR=/scratch/mu495/
 
 set queue=s48
 
 set counter = 0
-foreach option ($options)
-  setenv EVOLUTION $option
-  set jName = mc$counter
-  set logName = $OUTDIR/logs/${PHOTONFIELD}_${option}
-  qsub -q $queue $EXEDIR/bin/runProp.csh
-#-oo $logName.lsf.out -eo $logName.lsf.err -J $jName 
+#foreach option ($options)
+  export EVOLUTION=$option
+  jName=mc$counter
+  logName=$OUTDIR/logs/${PHOTONFIELD}_${option}
+  qsub -q $queue -o $logName.out -e $logName.err $EXEDIR/bin/runProp.csh
+# -J $jName
   @ counter ++
-end
+#end
