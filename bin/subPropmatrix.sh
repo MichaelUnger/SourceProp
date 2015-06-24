2425cd523ae4cdfa914fa405344a65f625951dfb
Michael Unger <11771724+MichaelUnger@users.noreply.github.com>
2015-06-24 04:12:21 +0000

    loop
    git-svn-id: https://svn.iap.kit.edu/svn/users/munger/GMF/Prop@3550 b4666b34-504e-453f-89ac-0a9f10f0e403

--- bin/subPropmatrix.sh
+++ bin/subPropmatrix.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#options = "SFR2 uniform uniformCutAt3 AGN AAGHRW05 M10 M15 M20 M25 M30 M35 M40 M45 M50"
+declare -a options=("SFR2" "uniform" "uniformCutAt3" "AGN" "AAGHRW05" "M10" "M15" "M20" "M25" "M30" "M35" "M40" "M45" "M50")
 
 option="uniform"
 export EXEDIR=/home/mu495/Software/Prop
@@ -11,11 +11,10 @@ export INDIR=/scratch/mu495/
 
 queue=s48
 
-set counter = 0
-#foreach option ($options)
+for option in "${options[@]}"
+do
   export EVOLUTION=$option
   logName=$OUTDIR/logs/${PHOTONFIELD}_${option}
   echo qsub -q $queue -o $logName.out -e $logName.err $EXEDIR/bin/runProp.sh
-  qsub -V -q $queue -o $logName.out -e $logName.err $EXEDIR/bin/runProp.sh
-#  @ counter ++
-#end
+#  qsub -V -q $queue -o $logName.out -e $logName.err $EXEDIR/bin/runProp.sh
+done
