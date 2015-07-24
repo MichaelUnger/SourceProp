e78ea755fc7d63e3b1652a765dd7a081ec4cc4a5
Michael Unger <11771724+MichaelUnger@users.noreply.github.com>
2015-07-24 02:27:57 +0000

    evolution
    git-svn-id: https://svn.iap.kit.edu/svn/users/munger/GMF/Prop@3582 b4666b34-504e-453f-89ac-0a9f10f0e403

--- bin/subScan.sh
+++ bin/subScan.sh
@@ -5,38 +5,43 @@ temperatures=("10" "15" "20" "25" "30" "35" "40" "45" "50" "60" "70" "80" \
     "300" "350" "400" "450" "500" "750" "1000" "2000" "3000" "4000"  "5000" \
     "6000" "7000" "8000" "9000")
 
+evolutions=("AAGHRW05" "AGN" "M10" "M15" "M20" "M25" "M30" "M35" "M40" "M45" "M50" "SFR2" "uniformCutAt3" "uniform")
+
 #temperatures=("250")
 
 export EXEDIR=/home/mu495/Software/Prop
 export OUTDIR=/scratch/mu495/Fit
 export DATADIR=$EXEDIR/Data
-export EVO=SFR2
 export IRB=G12
-export PRODNAME=Mass_${EVO}_${IRB}
 
-singleTemperature=0
+singleTemperature=1
 queue=s48
 
-nTemperature=${#temperatures[@]}
-echo number of temperatures $nTemperature
-((nTemperature--))
-
-i=0
-for temperature in "${temperatures[@]}"
+for evolution in "${evolutions[@]}"
 do
-  for j in $(seq $i $nTemperature)
+  export EVO=$evolution
+  export PRODNAME=Mix2_${EVO}_${IRB}
+  nTemperature=${#temperatures[@]}
+  echo number of temperatures $nTemperature
+  ((nTemperature--))
+
+  i=0
+  for temperature in "${temperatures[@]}"
   do
-    export T1=${temperatures[$j]}
-    export T2=$temperature
-    echo $T1 $T2
-    export FILEBASE=${PRODNAME}
-    logName=/home/mu495/Logfiles/${FILEBASE}_${T1}_${T2}
-    echo qsub -q $queue -o $logName.out -e $logName.err $EXEDIR/bin/runFit.sh
-    qsub -V -q $queue -o $logName.out -e $logName.err $EXEDIR/bin/runFit.sh
-    if [ $singleTemperature -ne 0 ]
-    then
-        break
-    fi
+    for j in $(seq $i $nTemperature)
+    do
+      export T1=${temperatures[$j]}
+      export T2=$temperature
+      echo $T1 $T2
+      export FILEBASE=${PRODNAME}
+      logName=/home/mu495/Logfiles/${FILEBASE}_${T1}_${T2}
+      echo qsub -q $queue -o $logName.out -e $logName.err $EXEDIR/bin/runFit.sh
+      qsub -V -q $queue -o $logName.out -e $logName.err $EXEDIR/bin/runFit.sh
+      if [ $singleTemperature -ne 0 ]
+      then
+          break
+      fi
+    done
+    ((i++))
   done
-  ((i++))
 done
