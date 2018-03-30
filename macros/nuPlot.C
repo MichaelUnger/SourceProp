da0dbb372f2a642d04b88bc58aea01725ca3b0ee
Michael Unger <11771724+MichaelUnger@users.noreply.github.com>
2018-03-30 18:20:22 +0000

    log
    git-svn-id: https://svn.iap.kit.edu/svn/users/munger/GMF/Prop@7891 b4666b34-504e-453f-89ac-0a9f10f0e403

--- macros/nuPlot.C
+++ macros/nuPlot.C
@@ -26,6 +26,15 @@ void
 nuPlot()
 {
   gStyle->SetOptLogy(1);
+
+  
+  TLegend* iceLegend = new TLegend(0.63, 0.186, 0.932, 0.386,NULL,"brNDCARC");
+  iceLegend->SetFillColor(0);
+  iceLegend->SetTextFont(42);
+  iceLegend->SetFillStyle(0);
+  iceLegend->SetBorderSize(0);
+
+  TGraph* ng = new TGraph("data/iceCube2017Limits.txt");
   TCanvas* c = new TCanvas("c", "", 10, 10, 1200, 500);
   TH2D* back = new TH2D("back", "", 100, 13, 18, 100, 1e-12, 1e-7);
   back->GetXaxis()->SetTitle("lg(E/eV)");
@@ -46,8 +55,10 @@ nuPlot()
   DrawNu("m=0", "Marco/pdfs/evo_0HistNu.root", kBlack, 2);
   DrawNu("m=+2", "Marco/pdfs/evo_p2HistNu.root", kRed-9, 2);
   DrawNu("m=+4", "Marco/pdfs/evo_p4HistNu.root", kRed+1, 2);
+  ng->Draw("CP");
+  iceLegend->AddEntry(ng, "IceCube 9yr 90% C.L.", "L");
   gLegend->Draw();
-
+  iceLegend->Draw();
   c->cd(2);
   gLegend = new TLegend(0.2, 0.7, 0.5, 0.9,NULL,"brNDCARC");
   gLegend->SetFillColor(0);
@@ -59,6 +70,7 @@ nuPlot()
   DrawNu("syst.", "Marco/pdfs/PRDSysHistNu.root", kBlack, 1, 2);
   DrawNu("Gal. mix", "Marco/pdfs/PRDGalacticHistNu.root", kBlack, 1, 3);
   DrawNu("low T", "Marco/pdfs/temp2Scan_100HistNu.root", kBlack, 1, 3);
+  ng->Draw("CP");
   gLegend->Draw();
   gPad->RedrawAxis();
 }
