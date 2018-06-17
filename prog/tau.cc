ae926771415d7fd09007ab1efecf026cac8006f5
Michael Unger <11771724+MichaelUnger@users.noreply.github.com>
2018-06-17 23:51:47 +0000

    optical depth of black body
    git-svn-id: https://svn.iap.kit.edu/svn/users/munger/GMF/Prop@8621 b4666b34-504e-453f-89ac-0a9f10f0e403

---
+++ prog/tau.cc
@@ -0,0 +1,133 @@
+#include "utl/Units.h"
+#include "utl/PhysicalConstants.h"
+#include "utl/MathConstants.h"
+
+using namespace utl;
+
+#include <TFile.h>
+#include <TH1D.h>
+
+#include <iostream>
+
+using namespace std;
+
+const double kSigmaT = 6.6524587158e-29*m2;
+
+double
+BlackBody(const double energy, const double temperature)
+{
+  const double kT = kBoltzmann * temperature;
+  const double preFac =
+    pow(kPi, 2) * pow(kPlanckReduced, 3) * pow(kSpeedOfLight, 3);
+  return pow(energy, 2) / preFac / (exp(energy/kT) - 1);
+}
+
+double
+SigmaGammaGamma(const double gammaCM2)
+{
+  if (gammaCM2 < 1)
+    return 0;
+  else if (gammaCM2 > 1e6)
+    return (log(2*gammaCM2) - 1) / gammaCM2 *  kSigmaT * 3/8;
+  const double betaCM2 = 1 - 1/gammaCM2;
+  const double betaCM4 = betaCM2 * betaCM2;
+  const double betaCM = sqrt(betaCM2);
+  if (betaCM < 1e-5) // Eq.(10.4)
+    return betaCM;
+
+  // Eq. (10.1), pi*r_e^2*8/3 = 3/16*sigma_T
+  const double t1 = (3 - betaCM4) * log((1 + betaCM)/(1 - betaCM));
+  const double t2 = 2 * betaCM * (2 - betaCM2);
+  return 3/16.* kSigmaT * (1 - betaCM2) * (t1 - t2);
+}
+
+double
+SigmaGammaGamma(const double projectileEnergy, const double targetEnergy,
+                const double cosTheta)
+{
+  const double mec2 = 0.5109989461*MeV;
+
+  // Dermer chapter 10
+  const double eps1 = projectileEnergy / mec2;
+  const double eps = targetEnergy / mec2;
+  const double mu = cosTheta;
+  const double gammaCM2 = 0.5 * (eps*eps1*(1-mu));
+  return SigmaGammaGamma(gammaCM2);
+}
+
+int
+main()
+{
+
+  // lg(E/eV) binning for target photon spectrum
+  const double nX = 1000;
+  const double x1 = -7;
+  const double x2 = 4;
+  const double dx = (x2 - x1) / nX;
+
+  // lg(E/eV) binning for gamma photon spectrum
+  const double nY = 500;
+  const double y1 = 5.5;
+  const double y2 = 24;
+  const double dy = (y2 - y1) / nY;
+  
+  TFile outFile("tau.root", "RECREATE");
+  TH1D hBB("hBB", "; lg(E/eV); dn/dE [m^{-3} eV^{-1};", nX, x1, x2);
+  TH1D hSigma("hSigma", "; E [eV]; #sigma_{e^{+}e^{-}}/(#pi r_e^{2});",
+              100, -0.5, 3);
+  TH1D hLambda("hLambda", "; lg(E/eV); #lambda} [Mpc];", nY, y1, y2);
+
+  const double T = 10*kelvin;
+
+  // reproduce Dermer Fig.10.1
+  for (int i = 0; i < hSigma.GetNbinsX(); ++i) {
+    const double lgGamma = hSigma.GetXaxis()->GetBinCenter(i+1);
+    const double gamma = pow(10, lgGamma);
+    const double sigma = SigmaGammaGamma(pow(gamma, 2));
+    const double piRe2 = kSigmaT * 3/8.;
+    hSigma.SetBinContent(i+1, sigma/piRe2);
+  }
+
+  // show black body
+  for (unsigned int i = 0; i < nX; ++i) {
+    const double lgE = x1 + i*dx + dx/2;
+    const double E = pow(10, lgE) * eV;
+    const double bb = BlackBody(E, T);
+    hBB.SetBinContent(i+1, bb / (1/cm3/eV));
+  }
+
+  // integrate for tau
+  const double cosMin = -1;
+  const double cosMax = 1;
+  const int nCos = 100;
+  const double dCos = (cosMax - cosMin) / nCos;
+  
+  for (unsigned int i = 0; i < nY; ++i) {
+    const double lgEgamma = y1 + i*dy + dy/2;
+    const double gammaEnergy = pow(10, lgEgamma) * eV;
+    double sumTarget = 0;
+    for (unsigned int j = 0; j < nX; ++j) {
+      const double lgEtarget = x1 + j*dx + dx/2;
+      const double targetEnergy = pow(10, lgEtarget) * eV;
+      const double n = BlackBody(targetEnergy, T);
+      double cosSum = 0;
+      for (unsigned int k = 0; k < nCos+1; ++k) {
+        const double cosTheta = cosMin + k*dCos;
+        const double sigma =
+          SigmaGammaGamma(gammaEnergy, targetEnergy, cosTheta);
+        cosSum += sigma;
+      }
+      const double nextLgEtarget = x1 + (j+1)*dx + dx/2;
+      const double nextTargetEnergy = pow(10, nextLgEtarget) * eV;
+      const double dE = nextTargetEnergy - targetEnergy;
+      sumTarget += (cosSum*dx*n*dE);
+    }
+    if (sumTarget > 1/(1000*Gpc))
+      hLambda.SetBinContent(i+1, 1./sumTarget/Mpc);
+  }
+  
+  outFile.Write();
+  outFile.Close();
+  
+  return 0;
+}
