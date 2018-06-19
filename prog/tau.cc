119829b56380d5d17d6a3a45208bf5b3e3caef29
Michael Unger <11771724+MichaelUnger@users.noreply.github.com>
2018-06-19 09:43:56 +0000

    tau
    git-svn-id: https://svn.iap.kit.edu/svn/users/munger/GMF/Prop@8637 b4666b34-504e-453f-89ac-0a9f10f0e403

--- prog/tau.cc
+++ prog/tau.cc
@@ -1,6 +1,11 @@
-#include "utl/Units.h"
-#include "utl/PhysicalConstants.h"
-#include "utl/MathConstants.h"
+/*
+ 
+  compile with g++ `root-config --cflags` tau.cc -o tau `root-config --libs`
+ 
+*/
+#include "Units.h"
+#include "PhysicalConstants.h"
+#include "MathConstants.h"
 
 using namespace utl;
 
@@ -11,8 +16,10 @@ using namespace utl;
 
 using namespace std;
 
-const double kSigmaT = 6.6524587158e-29*m2;
-
+/*
+  black body number density dn/dE [particles/energy/volume]
+  given temperature
+*/
 double
 BlackBody(const double energy, const double temperature)
 {
@@ -22,13 +29,18 @@ BlackBody(const double energy, const double temperature)
   return pow(energy, 2) / preFac / (exp(energy/kT) - 1);
 }
 
+
+/*
+  sigma(gamma+gamma -> e^+ + e^-) given
+  Lorentz-factor of CM system
+*/
 double
 SigmaGammaGamma(const double gammaCM2)
 {
   if (gammaCM2 < 1)
     return 0;
   else if (gammaCM2 > 1e6)
-    return (log(2*gammaCM2) - 1) / gammaCM2 *  kSigmaT * 3/8;
+    return (log(2*gammaCM2) - 1) / gammaCM2 *  kSigmaThomson * 3/8;
   const double betaCM2 = 1 - 1/gammaCM2;
   const double betaCM4 = betaCM2 * betaCM2;
   const double betaCM = sqrt(betaCM2);
@@ -38,9 +50,16 @@ SigmaGammaGamma(const double gammaCM2)
   // Eq. (10.1), pi*r_e^2*8/3 = 3/16*sigma_T
   const double t1 = (3 - betaCM4) * log((1 + betaCM)/(1 - betaCM));
   const double t2 = 2 * betaCM * (2 - betaCM2);
-  return 3/16.* kSigmaT * (1 - betaCM2) * (t1 - t2);
+  return 3/16.* kSigmaThomson * (1 - betaCM2) * (t1 - t2);
 }
 
+
+/*
+  sigma(gamma+gamma -> e^+ + e^-) given
+  - projectile photon energy
+  - target photon energy
+  - cos(theta) between projectile and target photon
+*/
 double
 SigmaGammaGamma(const double projectileEnergy, const double targetEnergy,
                 const double cosTheta)
@@ -55,14 +74,34 @@ SigmaGammaGamma(const double projectileEnergy, const double targetEnergy,
   return SigmaGammaGamma(gammaCM2);
 }
 
+
 int
 main()
 {
-
+  // ---- all these quantities must be in the same reference frame! ---
+  // black body temperature
+  const double T = 2.7*kelvin; //1e6*kelvin;
+  // ideal black body energy density 
+  const double uSB = 4*kStefanBoltzmann/kSpeedOfLight*pow(T, 4);
+  // desired energy density
+  const double uReference = uSB; //3.76 * erg/cm3;
+  // factor to dilute ideal black body
+  const double dilutionFactor = uReference / uSB;
+  // photon travel distance for calculation of tau
+  const double R = 2e17*cm/10; 
+  // gamma of observed frame -- only used to show tau(Eobs)
+  const double gamma = 10;
+  
+  cout << " T = " << T / kelvin << " K " << endl;
+  cout << " R = " << R / pc << " pc " << endl;
+  cout << " dilution factor = " << dilutionFactor << endl;
+  
   // lg(E/eV) binning for target photon spectrum
+  const double lgEpeak = log10(kBoltzmann*T/eV);
+  const double deltaLgE = 5;
   const double nX = 1000;
-  const double x1 = -7;
-  const double x2 = 4;
+  const double x1 = lgEpeak - deltaLgE;
+  const double x2 = lgEpeak + deltaLgE;
   const double dx = (x2 - x1) / nX;
 
   // lg(E/eV) binning for gamma photon spectrum
@@ -70,46 +109,68 @@ main()
   const double y1 = 5.5;
   const double y2 = 24;
   const double dy = (y2 - y1) / nY;
+
+  // avoid divide by 0
+  const double maxLambda = 1000*Gpc;
   
   TFile outFile("tau.root", "RECREATE");
-  TH1D hBB("hBB", "; lg(E/eV); dn/dE [m^{-3} eV^{-1};", nX, x1, x2);
-  TH1D hSigma("hSigma", "; E [eV]; #sigma_{e^{+}e^{-}}/(#pi r_e^{2});",
+  TH1D hBlackBody("hBlackBody", ";lg(E/eV);dn/dE [erg/cm^{-3};", nX, x1, x2);
+  TH1D hSigma("hSigma", ";#gamma_{CM};#sigma_{e^{+}e^{-}}/(#pi r_{e}^{2});",
               100, -0.5, 3);
-  TH1D hLambda("hLambda", "; lg(E/eV); #lambda} [Mpc];", nY, y1, y2);
-
-  const double T = 10*kelvin;
+  TH1D hLambda("hLambda", ";lg(E/eV);#lambda [Mpc];", nY, y1, y2);
+  TH1D hTau("hTau", ";lg(E_{BH}/eV);#tau;", nY,
+            y1 + log10(gamma) , y2 + log10(gamma));
 
   // reproduce Dermer Fig.10.1
   for (int i = 0; i < hSigma.GetNbinsX(); ++i) {
     const double lgGamma = hSigma.GetXaxis()->GetBinCenter(i+1);
     const double gamma = pow(10, lgGamma);
     const double sigma = SigmaGammaGamma(pow(gamma, 2));
-    const double piRe2 = kSigmaT * 3/8.;
+    const double piRe2 = kSigmaThomson * 3/8.;
     hSigma.SetBinContent(i+1, sigma/piRe2);
   }
 
-  // show black body
+  // show black body and calculate energy density
+  double sum = 0;
   for (unsigned int i = 0; i < nX; ++i) {
     const double lgE = x1 + i*dx + dx/2;
     const double E = pow(10, lgE) * eV;
     const double bb = BlackBody(E, T);
-    hBB.SetBinContent(i+1, bb / (1/cm3/eV));
+    const double dE = E*kLn10*dx;
+    sum += E*bb*dE;
+    hBlackBody.SetBinContent(i+1, bb / (erg/cm3));
   }
+  cout << " energy density: calc =             "         
+       << sum / (erg/cm3) << " erg/cm3,\n"
+       << "                 Stefan-Boltzmann = " << uSB / (erg/cm3)
+       << " erg/cm3" 
+       << endl;
 
-  // integrate for tau
-  const double cosMin = -1;
-  const double cosMax = 1;
-  const int nCos = 100;
-  const double dCos = (cosMax - cosMin) / nCos;
-  
+
+  // integrate 
+  cout << " integrating" << flush;
+
+  double tauMax = -1;
+  double lgEtauMax = 0;
+
+  // for all gamma energies
   for (unsigned int i = 0; i < nY; ++i) {
+    if (i%100 == 0)
+      cout << "." << flush;
     const double lgEgamma = y1 + i*dy + dy/2;
     const double gammaEnergy = pow(10, lgEgamma) * eV;
+    // integrate target energies
     double sumTarget = 0;
     for (unsigned int j = 0; j < nX; ++j) {
       const double lgEtarget = x1 + j*dx + dx/2;
       const double targetEnergy = pow(10, lgEtarget) * eV;
       const double n = BlackBody(targetEnergy, T);
+
+      // integrate cos(Theta)
+      const double cosMin = -1;
+      const double cosMax = 1;
+      const int nCos = 100;
+      const double dCos = (cosMax - cosMin) / nCos;
       double cosSum = 0;
       for (unsigned int k = 0; k < nCos+1; ++k) {
         const double cosTheta = cosMin + k*dCos;
@@ -117,15 +178,24 @@ main()
           SigmaGammaGamma(gammaEnergy, targetEnergy, cosTheta);
         cosSum += sigma;
       }
-      const double nextLgEtarget = x1 + (j+1)*dx + dx/2;
-      const double nextTargetEnergy = pow(10, nextLgEtarget) * eV;
-      const double dE = nextTargetEnergy - targetEnergy;
-      sumTarget += (cosSum*dx*n*dE);
+      const double dE = targetEnergy*kLn10*dx;
+      const double norm = 0.5; // from dn/dcos = 0.5 * n
+      sumTarget += (norm*cosSum*dx*n*dE);
     }
-    if (sumTarget > 1/(1000*Gpc))
-      hLambda.SetBinContent(i+1, 1./sumTarget/Mpc);
+    sumTarget *= dilutionFactor;
+    const double lambda = sumTarget > 1/maxLambda ?  1./sumTarget : maxLambda;
+    hLambda.SetBinContent(i+1, lambda/Mpc);
+    const double tau = R * sumTarget;
+    hTau.SetBinContent(i+1, tau);
+    if (tau > tauMax) {
+      tauMax = tau;
+      lgEtauMax = lgEgamma;
+    }
   }
-  
+  cout << endl;
+  cout << " tauMax = " << tauMax << " at lg(E/eV) = " << lgEtauMax << endl;
+
+  hLambda.SetMaximum(maxLambda/Mpc);
   outFile.Write();
   outFile.Close();
   
