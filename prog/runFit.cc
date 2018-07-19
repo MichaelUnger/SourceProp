ca3eac664a95173d64b318fc3c6fc73d70f9c13c
Marco Muzio <8448218+marcomuzio@users.noreply.github.com>
2018-07-19 17:48:00 +0000

    warp macro
    git-svn-id: https://svn.iap.kit.edu/svn/users/munger/GMF/Prop@8823 b4666b34-504e-453f-89ac-0a9f10f0e403

---
+++ prog/runFit.cc
@@ -0,0 +1,11 @@
+#include "../macros/fit.C"
+
+int main(int argc, char** argv)
+{
+   if (argc < 2) {
+      cerr << "usage: " << argv[0] << " <fitFile> " << endl;
+      return 1;
+   }
+   fit(argv[1]);
+   return 0;
+}
