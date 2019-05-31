7e395cdb22fa0543314a93f65eafbb718d6f63cb
Marco Muzio <8448218+marcomuzio@users.noreply.github.com>
2019-05-31 20:42:54 +0000

    Added new parameter enumerations for interpolation.
    git-svn-id: https://svn.iap.kit.edu/svn/users/munger/GMF/Prop@10151 b4666b34-504e-453f-89ac-0a9f10f0e403

--- src/FitParameters.h
+++ src/FitParameters.h
@@ -5,49 +5,40 @@
 
 namespace prop {
   enum EPar {
-    eLgEscFac,
-    eEscGamma,
-    eGamma,   
+    eGamma,   // 0
     eLgEmax,
-    eFGal,     
+    eLgEscFac, // 2
+    eEscGamma,
+    eFGal,     // 4
     eGammaGal,
-    eGammaGalLowE, 
+    eGammaGalLowE, // 6
     eDeltaGammaGal,
-    eLgEmaxGal,    
+    eLgEmaxGal,    // 8
     eNoPhoton,
-    eLgPhotonFieldFac, 
-    eExtraProtonLgFraction,
-    eExtraProtonLgEmax,
+    eLgPhotonFieldFac, // 10
+    eExtraProtonFraction195,
+    eExtraProtonLgEmax,  // 12
     eExtraProtonGamma,
     eExtraProtonMass,
     eExtraProtonLgRefE,
-    eUnused1,
-    eNpars,
-    eGammaA = eEscGamma + 1,
-    eDeltaGammaA = eEscGamma + 2,
-    eLgRmaxA = eEscGamma + 3,
-    eLgPhiA15 = eEscGamma + 4,
-    eGammaBl = eEscGamma + 5,
-    eDeltaGammaBl = eEscGamma + 6,
-    eLgRmaxBl = eEscGamma + 7,
-    eLgPhiBl17 = eEscGamma + 8,
-    eLgRmaxBd = eEscGamma + 9,
-    eLgPhiBd18 = eEscGamma + 10,
-    eGammaU = eEscGamma + 11,
-    eDeltaGammaU = eEscGamma + 12,
-    eLgRmaxU = eEscGamma + 13,
-    eLgPhiU19 = eEscGamma + 14,
-    eFacBU = eEscGamma + 15
+    eEvolutionM, // added by mmuzio
+    eEvolutionZ0, // mmuzio
+    eEvolutionDmin,
+    ePhotonPeak, // mmuzio
+    eNpars
   };
 
-  std::string
-  GetParLatexName(const EPar p, const bool boosted = false);
+  const
+  std::string&
+  GetParLatexName(const EPar p);
 
-  std::string
-  GetParName(const EPar p, const bool boosted = false);
+  const
+  std::string&
+  GetParName(const EPar p);
 
   EPar
-  GetPar(const std::string& parName, const bool boosted = false);
+  GetPar(const std::string& parName);
+
 
 }
 #endif
