1032cd036d1508481bbadc67590b6f8b979a9709
Marco Muzio <8448218+marcomuzio@users.noreply.github.com>
2019-05-31 20:41:27 +0000

    Added new naming information.
    git-svn-id: https://svn.iap.kit.edu/svn/users/munger/GMF/Prop@10150 b4666b34-504e-453f-89ac-0a9f10f0e403

--- src/FitParameters.cc
+++ src/FitParameters.cc
@@ -1,79 +1,43 @@
 #include "FitParameters.h"
 
 #include <stdexcept>
-#include <iostream>
 using namespace std;
 
 namespace prop {
 
-  string
-  GetParLatexName(const EPar p, const bool boosted)
+  const string&
+  GetParLatexName(const EPar p)
   {
-    string parNames[eNpars] =
-      {"lg(R_{esc}^{ Fe19})","#delta_{esc}",
-       "#gamma_{inj}", "lg(E_{max}^{ p}/eV)",
+    static const string parNames[eNpars] =
+      {"#gamma_{inj}", "lg(E_{max}^{ p}/eV)",
+       "lg(R_{esc}^{ Fe19})","#delta_{esc}",
        "f_{gal}", "#gamma_{gal}", "#gamma_{gal,0}", "#Delta#gamma_{gal}",
        "lg(E_{max}^{gal}/eV)",
-       "f_{noPhot}", "lg(fphot)", "lg(f(UHEp))", "lg(Emax, UHEp)",
-       "#gamma(UHEp)", "m_{extra}", "lg(E_{extra})", "N/A"};
-
-    if (boosted) {
-      parNames[eGammaA] = "\\gamma_{A}";
-      parNames[eGammaBl] = "\\gamma_{Bl}";
-      parNames[eGammaU] = "\\gamma_{U}";
-      parNames[eDeltaGammaA] = "\\Delta\\gamma_{A}";
-      parNames[eDeltaGammaBl] = "\\Delta\\gamma_{Bl}";
-      parNames[eDeltaGammaU] = "\\Delta\\gamma_{U}";
-      parNames[eLgPhiA15] = "lg\\Phi_{A}^{15}";
-      parNames[eLgPhiBl17] = "lg\\Phi_{Bl}^{17}";
-      parNames[eLgPhiBd18] = "lg\\Phi_{Bd}^{18}";
-      parNames[eLgPhiU19] = "lg\\Phi_{U}^{19}";
-      parNames[eLgRmaxA] = "lg(R_{max}^{A})";
-      parNames[eLgRmaxBl] = "lg(R_{max}^{Bl})";
-      parNames[eLgRmaxBd] = "lg(R_{max}^{Bd})";
-      parNames[eLgRmaxU] = "lg(R_{max}^{U})";
-      parNames[eFacBU] = "f_{B}^{U}";
-    }
+       "f_{noPhot}", "lg(fphot)", "f(UHEp)", "lg(Emax, UHEp)",
+       "#gamma(UHEp)", "m_{extra}", "lg(Eref, UHEp)",//};
+       "m_{evo}", "z_{0, evo}", "D_{min, evo}", "photonPeak"};
     return parNames[p];
   }
 
-  string
-  GetParName(const EPar p, const bool boosted)
+  const string&
+  GetParName(const EPar p)
   {
-    string parNames[eNpars] =
-      {"lgResc", "deltaEsc", "gammaInj", "lgRmax", 
+    static const string parNames[eNpars] =
+      {"gammaInj", "lgRmax", "lgResc", "deltaEsc",
        "fGal", "gammaGal", "gammaGalLowE", "deltaGammaGal",
-       "lgEmaxGal",  "fNoPhoton", "lgfPhoton", "extraProtonLgFraction",
+       "lgEmaxGal",  "fNoPhoton", "lgfPhoton", "extraProtonFraction",
        "extraProtonLgEmax", "extraProtonGamma", "extraProtonMass",
-       "extraProtonLgRefE", "NA"};
-
-    if (boosted) {
-      parNames[eGammaA] = "gammaA";
-      parNames[eGammaBl] = "gammaBl";
-      parNames[eGammaU] = "gammaU";
-      parNames[eDeltaGammaA] = "deltaGammaA";
-      parNames[eDeltaGammaBl] = "deltaGammaBl";
-      parNames[eDeltaGammaU] = "deltaGammaU";
-      parNames[eLgPhiA15] = "lgPhiA15";
-      parNames[eLgPhiBl17] = "lgPhiBl17";
-      parNames[eLgPhiBd18] = "lgPhiBd18";
-      parNames[eLgPhiU19] = "lgPhiU19";
-      parNames[eLgRmaxA] = "lgRmaxA";
-      parNames[eLgRmaxBl] = "lgRmaxBl";
-      parNames[eLgRmaxBd] = "lgRmaxBd";
-      parNames[eLgRmaxU] = "lgRmaxU";
-      parNames[eFacBU] = "facBU";
-    }
-
+       //"extraProtonLgRefE"};
+       "extraProtonLgRefE", "evolutionM", "evolutionZ0", "evolutionDmin", "photonPeak"};
     return parNames[p];
   }
 
   EPar
-  GetPar(const string& parName, const bool boosted)
+  GetPar(const string& parName)
   {
     for (unsigned int i = 0; i < eNpars; ++i) {
       EPar par = EPar(i);
-      if (parName == GetParName(par, boosted))
+      if (parName == GetParName(par))
         return par;
     }
     throw runtime_error("unknown par " + parName);
