5a78c1d6126d422b44481b10c00d1cc12fc614de
Marco Muzio <8448218+marcomuzio@users.noreply.github.com>
2019-06-04 18:22:34 +0000

    Added structures for evolution interpolation.
    git-svn-id: https://svn.iap.kit.edu/svn/users/munger/GMF/Prop@10194 b4666b34-504e-453f-89ac-0a9f10f0e403

--- src/PropMatrices.h
+++ src/PropMatrices.h
@@ -22,6 +22,7 @@ namespace prop {
     TMatrixD& GetMatrix(const int Aprim,
                         const int Asec);
     PrimaryMap& GetPrimaryMap() { return fMatrices; }
+    void ResetPrimaryMap();
     const PrimaryMap& GetPrimaryMap() const { return fMatrices; }
     SecondaryMap& GetSecondaryMap(const int Aprim)
     { return fMatrices.find(Aprim)->second; }
@@ -39,11 +40,29 @@ namespace prop {
     void SetMaximumDistance(const double dm)
     { fMaxDistance = dm; }
 
+    void UpdateMZ0(double oldM, double newM, double oldZ0, double newZ0);
+    PrimaryMap LoadInterpMatrixMZ0(double M, double Z0);
+    void InterpInitMZ0(double M, double Z0);
+    void InterpMZ0(double x, double xL, double xR, double y, double yD, double yU);
+
+    void UpdateDmin(double oldDmin, double newDmin);
+    PrimaryMap LoadInterpMatrixDmin(double Dmin);
+    void InterpInitDmin(double Dmin);
+    void InterpDmin(double x, double xL, double xR);
+
   private:
     double fLgEmin;
     double fLgEmax;
     double fMaxDistance;
     PrimaryMap fMatrices;
-  };
+    PrimaryMap InterpMatrixUL, InterpMatrixUR, InterpMatrixDL, InterpMatrixDR; // interpolation matrices U = up, D = down, L = left, R = right
+    double dM = 0.2, dZ0 = 0.25, Mmin = -5.0, Mmax = 5.0, Z0min = 0., Z0max = 5.0;
+    PrimaryMap InterpMatrixL, InterpMatrixR;
+    
+    // if these vectors are modified they must be initialized so that they are in ascending order numerically
+    std::vector<double> DminGrid = {0, 1, 5, 7, 10, 20, 30, 40, 50, 75, 100};
+    double DminMin = DminGrid.front(), DminMax = DminGrid.back();
+    int posR;
+   };
 }
 #endif
