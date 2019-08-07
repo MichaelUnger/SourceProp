8b5832f0cd9b3a6572d07be66b6b270d259ce554
Marco Muzio <8448218+marcomuzio@users.noreply.github.com>
2019-08-07 15:19:34 +0000

    Updated for hadronic interactions.
    git-svn-id: https://svn.iap.kit.edu/svn/users/munger/GMF/Prop@10324 b4666b34-504e-453f-89ac-0a9f10f0e403

--- src/VSource.h
+++ src/VSource.h
@@ -2,6 +2,7 @@
 #define _VSource_h_
 
 #include "Utilities.h"
+#include "HadronicInteractions.h"
 
 #include <cmath>
 #include <iostream>
@@ -12,6 +13,10 @@ namespace prop {
     enum EProcess {
       ePD,
       ePP
+    };
+    enum EChannel {
+      ePH,
+      eH
     };
   public:
 
@@ -24,11 +29,19 @@ namespace prop {
     
     void SetEscFac(const double f) { fEscFac = f; }
     void SetEscGamma(const double g) { fEscGamma = g; }
+    void SetHadIntFac(const double h) { HadInts->SetHadIntRatio(h);}
 
     virtual
     double
-    LambdaInt(const double /*E*/, const int /*A*/)
+    LambdaPhotoHadInt(const double /*E*/, const int /*A*/)
       const = 0;
+
+    double
+    LambdaHadInt(const double E, const int Aprim)
+      const
+    {
+	return HadInts->LambdaHadInt(E, Aprim);
+    }
 
     double
     LambdaEsc(const double E, const double A)
@@ -51,6 +64,34 @@ namespace prop {
     double
     GetPDBranchingRatio(const double /*E*/, const int /*Asec*/, const int /*Aprim*/)
       const = 0;
+    
+    virtual
+    double
+    GetMPPBranchingRatio(const double /*E*/, const int /*Aprim*/) 
+      const = 0;
+
+    virtual
+    double
+    LambdaPPInt(const double /*E*/, const int /*Aprim*/)
+      const = 0;
+
+    virtual
+    double
+    PartialLambdaSPPInt(const double /*E*/, const int /*Aprim*/, const double /*epsMax*/) 
+      const = 0;
+
+    virtual
+    double
+    PartialLambdaMPPInt(const double /*E*/, const int /*Aprim*/, const double /*epsMax*/) 
+      const = 0;
+    
+    double GetNSecondaries(const double Esec, const double Eprim, const int Asec, const int Aprim) const { return HadInts->GetNSecondaries(Esec, Eprim, Asec, Aprim); }
+
+    double GetNByPDGID(const double Esec, const double Eprim, const int pdgID, const int Aprim) const { return HadInts->GetNByPDGID(Esec, Eprim, pdgID, Aprim); }
+
+    int GetPDGID(std::string name) const { return HadInts->GetPDGID(name); }
+
+    int GetPDGID(const int A, const int Z, bool isAntimatter = false) { return HadInts->GetPDGID(A, Z, isAntimatter); }
 
     virtual
     double
@@ -59,8 +100,19 @@ namespace prop {
       const = 0;
 
     virtual
+    double
+    GetChannelFraction(const double E, const int A,
+                       const EChannel p)
+      const = 0;
+
+    virtual
     void
     Update(double newPeak) = 0;
+
+    virtual
+    double 
+    GetMeanPhotonEnergy()
+      const = 0;
 
     void SetPhotonScaleFactors(const std::vector<double>& f)
     { fFieldScaleFactors = f; }
@@ -69,6 +121,7 @@ namespace prop {
     std::vector<double> fFieldScaleFactors;
     double fEscFac;
     double fEscGamma;
+    HadronicInteractions* HadInts;
   };
 }
 
