e6aa19b3973a71955026546bfcb65cc53759f64e
Marco Muzio <8448218+marcomuzio@users.noreply.github.com>
2019-08-07 15:14:12 +0000

    Added to link hadronic interactions.
    git-svn-id: https://svn.iap.kit.edu/svn/users/munger/GMF/Prop@10319 b4666b34-504e-453f-89ac-0a9f10f0e403

---
+++ src/HadronicInteractionsLinkDef.h
@@ -0,0 +1,15 @@
+#ifndef _HadronicInteractionsLinkDef_h_
+#define _HadronicInteractionsLinkDef_h_
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class prop::HadronicInteractions+;
+//#pragma link C++ typdef prop::HadronicInteractions::SecondaryMatrix+;
+//#pragma link C++ class map<int, TH2D*>+;
+#endif
+
+#endif // CINT
