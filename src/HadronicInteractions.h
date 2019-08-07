885c9d941255255db67345b19155c96777d9293f
Marco Muzio <8448218+marcomuzio@users.noreply.github.com>
2019-08-07 15:13:11 +0000

    Methods for handling hadronic interactions and building interaction matrices.
    git-svn-id: https://svn.iap.kit.edu/svn/users/munger/GMF/Prop@10318 b4666b34-504e-453f-89ac-0a9f10f0e403

---
+++ src/HadronicInteractions.h
@@ -0,0 +1,53 @@
+#ifndef _HadronicInteractions_h_
+#define _HadronicInteractions_h_
+
+//#include "VSource.h"
+
+#include <TTree.h>
+#include <TH2D.h>
+
+#include <map>
+#include <string>
+#include <vector>
+
+namespace prop {
+
+  class HadronicInteractions {
+
+  public:
+    HadronicInteractions(const std::string& modelName, const std::string& directory);
+    virtual ~HadronicInteractions();
+
+    void SetHadIntRatio(double f);
+
+    TH2D* GetMatrix(const int Aprim, const int Asec); 
+    double GetsigmaInel(const int Aprim, const double lgE);
+
+    double LambdaHadInt(const double E, const int Aprim);
+    double GetNSecondaries(const double Esec, const double Eprim, const int Asec, const int Aprim);
+    double GetNByPDGID(const double Esec, const double Eprim, const int pdgID, const int Aprim);
+    std::vector<int> GetSecondaryIDs(const int Aprim);
+    int GetPDGID(std::string name);
+    int GetPDGID(const int A, const int Z, bool isAntimatter);
+
+  private:
+    void ReadHI();
+
+    typedef std::map<int, TH2D*> SecondaryMatrix;
+    typedef std::map<int, SecondaryMatrix> PrimaryMatrix;
+    typedef std::map<int, TH1D*> sigmaInelMap;
+
+    const double lgEmin = 15., lgEmax = 21.;
+    const double lgEsecmin = 12., lgEsecmax = 21.;
+
+    PrimaryMatrix fMatrix;
+    sigmaInelMap fsigmaInel;
+    const std::string fModelName;
+    const std::string fDirectory;
+    double fHadIntRatio;
+
+    ClassDef(HadronicInteractions, 1)
+
+  };
+}
+#endif
