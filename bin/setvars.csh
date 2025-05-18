f981521cea9b2db6edc7c244f057d355f5a3a7d4
Michael Unger <michael.unger@kit.edu>
2025-05-18 13:14:02 +0200

    remove scripts

--- bin/setvars.csh
+++
@@ -1,5 +0,0 @@
-if ($?LD_LIBRARY_PATH) then
-  setenv LD_LIBRARY_PATH ${LD_LIBRARY_PATH}:$PWD/lib
-else
-  setenv LD_LIBRARY_PATH $PWD/lib
-endif
--- bin/setvars_butinah.sh
+++
@@ -1,5 +0,0 @@
-export PROPROOT=/home/mu495/Software/Prop
-export PATH=$PROPROOT/bin:$PATH
-export LD_LIBRARY_PATH=$PROPROOT/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=$CRPROPAROOT/lib/python2.7/site-packages:$PYTHONPATH
-
